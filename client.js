e234d2f740150ce64877fb0dd8ad5b2b9183c861
Chris Johnson <crjjrc@gmail.com>
2018-12-20 06:09:08 +1000

    Add image loading through argv.

--- client.js
+++ client.js
@@ -1,16 +1,19 @@
 let canvas;
 let gl;
-let shaderProgram;
-let vao;
+
+let backgroundProgram;
+let backgroundVao;
 let backgroundTexture;
+
+let imageProgram;
+let imageVao;
+let imageTexture;
+
 let backgroundTextureUniform;
+let imageTextureUniform;
 let resolutionUniform;
 
-function onReady() {
-  canvas = document.getElementById('canvas');
-  gl = canvas.getContext('webgl2');
-  onSize();
-
+function createBackground() {
   let vertexSource = `#version 300 es
 uniform vec2 resolution;
 in vec4 position;
@@ -36,7 +39,7 @@ void main() {
 
   let vertexShader = compileShader(gl.VERTEX_SHADER, vertexSource);
   let fragmentShader = compileShader(gl.FRAGMENT_SHADER, fragmentSource);
-  shaderProgram = linkProgram(vertexShader, fragmentShader);
+  backgroundProgram = linkProgram(vertexShader, fragmentShader);
 
   let positions = [
     -1.0, -1.0, 0.0, 1.0,
@@ -45,13 +48,13 @@ void main() {
      1.0,  1.0, 0.0, 1.0
   ];
 
-  let positionAttributeLocation = gl.getAttribLocation(shaderProgram, 'position');
+  let positionAttributeLocation = gl.getAttribLocation(backgroundProgram, 'position');
   let positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-  vao = gl.createVertexArray();
-  gl.bindVertexArray(vao);
+  backgroundVao = gl.createVertexArray();
+  gl.bindVertexArray(backgroundVao);
   gl.enableVertexAttribArray(positionAttributeLocation);
   gl.vertexAttribPointer(positionAttributeLocation, 4, gl.FLOAT, false, 0, 0);
 
@@ -63,24 +66,85 @@ void main() {
     lite, lite, lite, 255,
     dark, dark, dark, 255,
   ]);
-  backgroundTexture = createTexture(gl, checker, 2, 2);
 
-  let backgroundTextureUniform = gl.getUniformLocation(shaderProgram, 'backgroundTexture');
   gl.activeTexture(gl.TEXTURE0);
-  gl.bindTexture(gl.TEXTURE_2D, backgroundTexture);
+  backgroundTexture = createTexture(gl, 2, 2, 4, checker);
 
-  resolutionUniform = gl.getUniformLocation(shaderProgram, 'resolution');
+  backgroundTextureUniform = gl.getUniformLocation(backgroundProgram, 'backgroundTexture');
+  resolutionUniform = gl.getUniformLocation(backgroundProgram, 'resolution');
 
-  gl.useProgram(shaderProgram);
+  gl.useProgram(backgroundProgram);
   gl.uniform1i(backgroundTextureUniform, 0);
-
-  render();
+  gl.useProgram(null);
 }
 
-function createTexture(gl, pixels, width, height) {
+function createImage() {
+  let vertexSource = `#version 300 es
+uniform vec2 resolution;
+in vec4 position;
+out vec2 fTexCoords;
+
+void main() {
+  gl_Position = position;
+  fTexCoords = position.xy * 0.5 + 0.5;
+}
+  `;
+
+  let fragmentSource = `#version 300 es
+precision mediump float;
+uniform sampler2D imageTexture;
+in vec2 fTexCoords;
+out vec4 fragmentColor;
+
+void main() {
+  // fragmentColor = vec4(fTexCoords, 0.0, 1.0);
+  fragmentColor = texture(imageTexture, fTexCoords);
+}
+  `; 
+
+  let vertexShader = compileShader(gl.VERTEX_SHADER, vertexSource);
+  let fragmentShader = compileShader(gl.FRAGMENT_SHADER, fragmentSource);
+  imageProgram = linkProgram(vertexShader, fragmentShader);
+
+  let scale = 0.9;
+  let positions = [
+    -scale, -scale, 0.0, 1.0,
+     scale, -scale, 0.0, 1.0,
+    -scale,  scale, 0.0, 1.0,
+     scale,  scale, 0.0, 1.0
+  ];
+
+  let positionAttributeLocation = gl.getAttribLocation(imageProgram, 'position');
+  let positionBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  imageVao = gl.createVertexArray();
+  gl.bindVertexArray(imageVao);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.vertexAttribPointer(positionAttributeLocation, 4, gl.FLOAT, false, 0, 0);
+
+  imageTextureUniform = gl.getUniformLocation(imageProgram, 'imageTexture');
+
+  gl.useProgram(imageProgram);
+  gl.uniform1i(imageTextureUniform, 1);
+  gl.useProgram(null);
+}
+
+function onReady() {
+  canvas = document.getElementById('canvas');
+  gl = canvas.getContext('webgl2');
+  createBackground();
+  createImage();
+  render();
+
+  onSize();
+}
+
+function createTexture(gl, width, height, nchannels, pixels) {
   let texture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, gl.RGBA, gl.UNSIGNED_BYTE, pixels);
+  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, width, height, 0, nchannels == 4 ? gl.RGBA : gl.RGB, gl.UNSIGNED_BYTE, pixels);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
   return texture;
@@ -91,12 +155,20 @@ function render() {
   gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.useProgram(shaderProgram);
+  gl.useProgram(backgroundProgram);
   gl.uniform2f(resolutionUniform, gl.canvas.width, gl.canvas.height);
-  gl.bindVertexArray(vao);
+  gl.bindVertexArray(backgroundVao);
   gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
   gl.bindVertexArray(null);
   gl.useProgram(null);
+
+  if (imageTexture) {
+    gl.useProgram(imageProgram);
+    gl.bindVertexArray(imageVao);
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+    gl.bindVertexArray(null);
+    gl.useProgram(null);
+  }
 }
 
 function linkProgram(vertexShader, fragmentShader) {
@@ -128,10 +200,25 @@ function compileShader(type, source) {
 }
 
 function onSize() {
+  console.log("sizing");
   canvas.width = canvas.clientWidth; 
   canvas.height = canvas.clientHeight; 
   render();
 }
+
+function loadTexture(width, height, nchannels, pixels) {
+  console.log("width:", width);
+  console.log("height:", height);
+  console.log("nchannels:", nchannels);
+
+  gl.activeTexture(gl.TEXTURE1);
+  imageTexture = createTexture(gl, width, height, nchannels, pixels);
+  render();
+}
+
+require('electron').ipcRenderer.on('loadTexture', (event, width, height, nchannels, pixels) => {
+  loadTexture(width, height, nchannels, pixels);
+});
 
 window.addEventListener('load', onReady);
 window.addEventListener('resize', onSize, false);
--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const { app, BrowserWindow } = require('electron');
+const sharp = require('sharp');
 
 function createWindow() {
   var browser = new BrowserWindow({ width: 800, height: 600 });
   browser.loadFile('index.html');
+
+  let path = process.argv[2];
+  browser.webContents.on('did-finish-load', () => {
+    console.log("loading");
+    sharp(path)
+      .raw()
+      .toBuffer((error, data, info) => {
+        console.log("error:", error);
+        console.log("data:", data);
+        console.log("info:", info);
+        browser.webContents.send('loadTexture', info.width, info.height, info.channels, data);
+      });
+  });
 }
 
 app.on('ready', createWindow);
